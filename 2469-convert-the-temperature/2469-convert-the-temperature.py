20b39c53f4b7085615fb90ed59bf09c0d16b7c01
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-04-24 09:38:14 +0300

    Time: 39 ms (23.03%), Space: 16.5 MB (42.68%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -0,0 +1,7 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        Kelvin = celsius + 273.15
+        Fahrenheit = celsius * 1.80 + 32.00
+        ans=[Kelvin,Fahrenheit]
+        return ans
+#
