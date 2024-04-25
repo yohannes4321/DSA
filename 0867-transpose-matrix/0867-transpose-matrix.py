249dc1fca265ba2737ee6c4b033ac129d77c1a8d
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-04-25 19:27:52 +0300

    Time: 70 ms (34.01%), Space: 17.4 MB (21.65%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -1,9 +1,10 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
     #lets crete anoter matrix wirh zero
-        res=[[0]*len(matrix) for _ in range(len(matrix[0]))] 
-        # which is for every coloumn in the loop when iterates we gona create a [0] woth 
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                res[c][r]=matrix[r][c]
+        res=[]
+        for j in range(len(matrix[0])):
+            temp=[]
+            for i in range(len(matrix)):
+                temp.append(matrix[i][j])
+            res.append(temp)
         return res
