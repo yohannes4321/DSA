78edeb98d32b7e0acad50e743e3b16ef1ed8fa00
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-05-05 15:43:06 +0300

    Time: 534 ms (53.03%), Space: 53.6 MB (64.60%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
@@ -1,13 +1,11 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
     
-        modified_chars = []  # List to store modified characters
-        space_idx = 0  # Index for the spaces list
-
-        for i, char in enumerate(s):
-            if space_idx < len(spaces) and i == spaces[space_idx]:
-                modified_chars.append(' ')  # Add a space before the character
-                space_idx += 1
-            modified_chars.append(char)  # Add the character
-
-        return ''.join(modified_chars)
+        modeifed_c =[]
+        space_index=0
+        for i in range(len(s)):
+            if space_index < len(spaces) and i == spaces[space_index]:
+                modeifed_c.append(" ")
+                space_index +=1
+            modeifed_c.append(s[i])
+        return "".join(modeifed_c)
