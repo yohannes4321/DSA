207367a594275e662731e27d2e865433b63a0f44
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-05-05 12:40:06 +0300

    Time: 584 ms (97.89%), Space: 32.6 MB (7.58%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
@@ -1,17 +1,18 @@
 class Solution:
     def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()  # Sort the players by ability
+        trainers.sort()  # Sort the trainers by training capacity
         
-
-        players.sort()
-        trainers.sort()
-
-        p, t = 0, 0
-        matchings = 0
-
-        while p < len(players) and t < len(trainers):
-            if players[p] <= trainers[t]:
-                matchings += 1
-                p += 1
-            t += 1
-
-        return matchings
+        match_count = 0  # To keep track of the number of matchings
+        p_ptr, t_ptr = 0, 0  # Pointers to iterate through players and trainers
+        
+        while p_ptr < len(players) and t_ptr < len(trainers):
+            # If the player's ability is less than or equal to the trainer's training capacity
+            if players[p_ptr] <= trainers[t_ptr]:
+                match_count += 1  # Increment the match count
+                p_ptr += 1  # Move to the next player
+                t_ptr += 1  # Move to the next trainer
+            else:
+                t_ptr += 1  # Move to the next trainer, as this trainer can't match the current player
+        
+        return match_count
