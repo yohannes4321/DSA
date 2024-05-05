2821f4cc5e71ebb1130d091899b5bd89a2d7df48
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-05-05 10:43:45 +0300

    Time: 363 ms (20.02%), Space: 23.4 MB (25.27%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,23 +1,17 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         
-        people.sort()  # Sort the people's weights in ascending order
-        left, right = 0, len(people) - 1
-        count = 0
-
-        while left <= right:
-            # Check if the sum of weights at the current pointers is within the limit
-            if people[left] + people[right] <= limit:
-                count += 1
-                left += 1
-                right -= 1
-            # If the weight at the left pointer exceeds the limit, move the left pointer
-            elif people[left] >= limit:
-                count += 1
-                left += 1
-            # Otherwise, move the right pointer
-            else:
-                count += 1
-                right -= 1
-
-        return count
+        people.sort()
+        left =0
+        count =0
+        right=len(people)-1
+        while left <=right:
+            if people[left]+people[right] <= limit :
+                
+                left +=1
+                right -=1
+            else: 
+                  
+                right -=1
+            count +=1 
+        return count 
