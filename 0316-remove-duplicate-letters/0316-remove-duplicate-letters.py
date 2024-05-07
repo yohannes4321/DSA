4959999876082ef71cfb5a293814e668f269f57a
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-05-07 16:30:53 +0300

    Time: 42 ms (37.69%), Space: 16.7 MB (48.27%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,23 +1,22 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        freq = [0] * 26  # Track last index of occurrence for each element
+       # first is stack is monotonically increasing because with lexicorgraphical order
+    # if the stack[-1] before we pop() we have to chaeck if it does occur in the future unless we donot pop() it also contradict we increading monotonic stack
+        # when we pop() we must change the seen to false
+    # before we add to stack we have to check if the seen is false becuase it is uniq charcters
+        freq=[0]*26
         for i in range(len(s)):
-            freq[ord(s[i]) - ord('a')] += 1
-        
-        stack = []  # Monotonic stack to maintain increasing order of chars
-        seen = [False] * 26  # Track already included elements
+            freq[ord(s[i])- ord('a')] +=1 # from 0 to 25 
+        seen=[False] *26
+        stack=[]
         for i in range(len(s)):
-            if seen[ord(s[i]) - ord('a')]:
-                # Don't process already included char
-                freq[ord(s[i]) - ord('a')] -= 1
+            if seen[ord(s[i])- ord('a')]:
+                freq[ord(s[i]) -ord('a')] -=1
                 continue
-            while stack and stack[-1] > s[i] and freq[ord(stack[-1]) - ord('a')] > 0:
-                # Pop all possible larger chars
-                seen[ord(stack[-1]) - ord('a')] = False
+            while stack and stack[-1] > s[i] and freq[ord(stack[-1])-ord('a')] >0:
+                seen[ord(stack[-1])- ord('a')] = False
                 stack.pop()
             stack.append(s[i])
-            seen[ord(s[i]) - ord('a')] = True
-            freq[ord(s[i]) - ord('a')] -= 1
-        
-        # Build answer string
-        return "".join(stack)
+            freq[ord(s[i])-ord('a')]-=1
+            seen[ord(s[i])-ord('a')]=True
+        return ''.join(stack)
