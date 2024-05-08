d26dd04ab297215153d7ff1be586764861260b55
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-05-08 10:16:47 +0300

    Time: 50 ms (51.79%), Space: 16.8 MB (44.70%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,17 +1,17 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hash_map={nums:index for index,nums in enumerate (nums1)}
-        res=[-1]*(len(nums1))
+        hash_map={value:key for key ,value in enumerate(nums1)}
         stack=[]
+        res=[-1]*len(nums1)
         for i in range(len(nums2)):
-            current=nums2[i]
-            while stack and current > stack[-1]:# this show us that current value is greatter than hte stack top value and we use while loop to go from the top to bottom 
+            while stack and nums2[i]>stack[-1]:
                 n=stack.pop()
-                ind=hash_map[n]
-                res[ind]=current
-                print(current)
-            if current in hash_map:
-                stack.append(current)
-        return res
+                index=hash_map[n]
+                res[index]=nums2[i]
                 
-        
+            if nums2[i] in hash_map:
+                stack.append(nums2[i])
+            
+        return res
+    
+    
