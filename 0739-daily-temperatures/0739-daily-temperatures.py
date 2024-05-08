8144687e3e6066fe8a31e244cbd4e90b0dfaf133
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-05-08 10:48:13 +0300

    Time: 892 ms (61.77%), Space: 31.9 MB (28.29%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,18 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []  # Initialize an empty stack to keep track of indices
-        res = [0] * len(temperatures)  # Initialize the result list with zeros
-
+        #we cannot use hashmap because there isnot unque
+        stack=[]
+        res=[0]*len(temperatures)
+        hash_map={value:key for key ,value in enumerate(temperatures)}
         for i in range(len(temperatures)):
-            current = temperatures[i]
-
-            # Pop elements from the stack while the current temperature is greater
-            # than the temperature at the top of the stack
-            while stack and current > temperatures[stack[-1]]:
-                prev_index = stack.pop()
-                res[prev_index] = i - prev_index
-
-            # Push the current index onto the stack
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                n=stack.pop()# this is index value 
+                res[n]=i-n
             stack.append(i)
-
         return res
+            
