0497ba2d6f53f32bfac59049d4fd9c72fada1d87
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-05-14 14:39:19 +0300

    Time: 38 ms (57.09%), Space: 16.6 MB (41.22%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -1,32 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        length=0
         if not head:
             return None
-
-        # Calculate the length of the linked list
-        length = 0
-        current = head
+        current =b=head
         while current:
-            current = current.next
-            length += 1
-
-        # Handle the case where no rotation is needed
-        index = k % length
-        if index == 0:
+            
+            current=current.next
+            length+=1
+            
+        
+        index=k %length
+        if index ==0:
             return head
-
-        # Find the new head after rotation
-        b = head
-        for i in range(length - index - 1):
-            b = b.next
-
-        new_head = b.next
-        b.next = None
-
-        # Connect the last element to the original head
-        current = new_head
-        while current.next:
-            current = current.next
-        current.next = head
-
+        
+        
+        for i in range(length-index-1):
+            b=b.next
+        new_head=b.next
+        b.next=None
+        curr=new_head
+        while curr.next:
+            curr=curr.next
+        curr.next=head
         return new_head
+        
+            
+            
