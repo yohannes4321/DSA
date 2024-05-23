2035b22be62d23d977cf3854f64e059dced29c7c
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-05-23 17:05:37 +0300

    Time: 2289 ms (19.73%), Space: 16.4 MB (98.27%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -7,6 +7,6 @@ class Solution:
             score_left=nums[left] - max_score_diff(nums,left+1,right)
             score_right=nums[right] - max_score_diff(nums,left,right -1 )
             return max(score_left , score_right)
-        
+        max_diff=-1
         max_diff=max_score_diff(nums,0,len(nums)-1)
         return  max_diff>=0
