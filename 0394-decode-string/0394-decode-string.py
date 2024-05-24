30d50e4906eaa8d46f2a2c1d4d6b08ecab31ccfe
yohannes4321 <138892760+yohannes4321@users.noreply.github.com>
2024-05-24 18:30:23 +0300

    Time: 22 ms (99.24%), Space: 16.5 MB (92.07%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -2,19 +2,18 @@ class Solution:
     def decodeString(self, s: str) -> str:
         stack=[]
         for i in s:
-            if i != ']':
+            if i !=']':
                 stack.append(i)
             else:
                 string=""
-                while stack[-1] !='[':
+                while stack[-1]!='[':
                     
                     string=stack.pop()+string
+                #to pop the '['
                 stack.pop()
-                # this for number 
                 number=""
                 while stack and stack[-1].isdigit():
                     number=stack.pop()+number
-                
-                repeting_string=int(number)*string
-                stack.append(repeting_string)
+                string_mul=string*int(number)
+                stack.append(string_mul)
         return ''.join(stack)
