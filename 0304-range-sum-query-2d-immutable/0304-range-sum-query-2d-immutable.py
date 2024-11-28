83d41cb496208c054fddb26841dc72703d3852cf
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2024-11-28 14:33:34 +0300

    Time: 92 ms (89.63%), Space: 30 MB (36.79%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,28 +1,80 @@
 class NumMatrix:
 
-    def __init__(self, matrix: List[List[int]]):
-        # here we gona add the value here only the summation is box 
-        row=len(matrix) # to add 0s on the top    ###$$$$  this is for res the list we have created 
-        column=len(matrix[0])# to add 0s on the left
-        #createing a res
-        self.res=[[0]*(column +1) for _ in range(row+1)]
-        for i in range(len(matrix)):
-            linesum=0
-            for j in range(len(matrix[0])): 
-                linesum+=matrix[i][j] # the summmation of the line only there is no above 
-                above=self.res[i][j+1]
-                self.res[i+1][j+1]=linesum+above
- 
-                
-        
+    """def __init__(self, matrix: List[List[int]]):
+        ROWS,COLS=len(matrix),len(matrix[0])
+        self.sumMatrix=[[0] *(COLS+1) for r in range(ROWS +1 )]
+
+        for r in range(ROWS):
+            prefix=0 # for every line we must solve prefix of sum start from zero 
+            for c in range(COLS):
+                prefix += matrix[r][c]
+                above= self.sumMatrix [r][c+1]
+                self.sumMatrix[r+1][c+1]=prefix + above 
+
+     
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        row1,col1,row2,col2= row1+1,col1+1,row2+1,col2+1 # this is for res list we increment 1
-        abovee=self.res[row1-1][col2]
-        left=self.res[row2][col1-1]
-        bottomright=self.res[row2][col2]
-        intersection=self.res[row1-1][col1-1]
-        return bottomright-abovee-left+intersection
+        r1,c1,r2,c2=row1 +1,col1+1,row2+1 ,col2+1
+        bottomright=self.sumMatrix[r2][c2]
+        left=self.sumMatrix[r1-1][c2]
+        above=self.sumMatrix[r2][c1-1]
+        lefttop=self.sumMatrix[r1-1][c1-1]
+        return bottomright-left-above+lefttop"""
+    def __init__(self,matrix: List[List[int]]):
+        ROWS,COLS=len(matrix),len(matrix[0])
+        self.sum_matrix=[[0]*(COLS+1) for r in range(ROWS+1)]
+
+        # INTIALIZE THE SUM MATRIX 
+        # THIS IS FOR THE FORMER MATRIX WHICH DOES NOT ADD THE LEFT AND RIGHT ZERO
+        for r in range(ROWS) :
+            prefix=0
+            for c in range(COLS):
+                prefix+=matrix[r][c]
+                above=self.sum_matrix[r][c+1] # because it is not added
+                self.sum_matrix[r+1][c+1]=prefix+above
+    def sumRegion(self,row1,col1,row2,col2):
+        r1,c1,r2,c2=row1+1,col1+1,row2+1,col2+1 
+        thewholematrix=self.sum_matrix[r2][c2]
+        leftmatrix=self.sum_matrix[r2][c1-1]
+        abovematrix=self.sum_matrix[r1-1][c2]
+        topleft=self.sum_matrix[r1-1][c1-1]
+
+        return thewholematrix - leftmatrix - abovematrix + topleft
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Your NumMatrix object will be instantiated and called as such:
