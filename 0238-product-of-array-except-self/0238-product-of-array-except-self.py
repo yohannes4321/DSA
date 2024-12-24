5926602c48188b1625384ae91c9cc57a8e04ea7f
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2024-12-24 19:43:10 +0300

    Time: 28 ms (82.22%), Space: 20.3 MB (45.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,15 +1,20 @@
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # i am gona use two array the prefix and suffix array
- 
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        output=[1]* len(nums)
+        #for prefix
         prefix=1
-        suffix =1
-        res=[1]*len(nums)
         for i in range(len(nums)):
-            res[i]=prefix
-            prefix *=nums[i]
-        for j in range(len(nums)-1,-1,-1): # backward going in reaverse diraction 
-            res[j]*=suffix # to replace it (we donot want to overwrite it ) we want it  to multiply and put  the product in the postion 
-            suffix*=nums[j]
-        return res
             
+            output[i]=prefix
+            prefix*=nums[i]
+            #  i have modifyed the predix by multiplying the prefix with current num
+        suffix=1
+        for i in range(len(nums)-1,-1,-1):
+            
+            output[i]*=suffix
+            suffix*=nums[i]
+        return output
