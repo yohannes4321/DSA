39a6233674fbc87d2ee9b317a369472d7aa8f5ed
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-01-30 11:57:39 +0300

    Time: 3 ms (29.29%), Space: 17.5 MB (83.52%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,11 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        #let assume that the first string is shortes from all and if it termainates all 
-        #will terminate f
-        res=""
-        for i in range(len(strs[0])):#let assume for this time that the first string is shortes of all if not we will check it later
-            for j in range(len(strs)):
-                if i >= len(strs[j]) or strs[0][i]!=strs[j][i]:
-                    return res
-            res+=strs[0][i]
-        return res
+        str_t=""
+        word=strs[0]
+        for i in range(len(word)):
+            for j in range(1,len(strs)):
+                if i == len(strs[j]) or word[i]!= strs[j][i]:
+                    return str_t
+            str_t+=word[i]
+         
+        return str_t
+
+
+
+
+                
