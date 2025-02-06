a656d3b92289772cbb6f7ac776ea063ad80e1267
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-06 21:35:46 +0300

    Created 'Length of Last Word' by A2SV Hub

---
+++ 06-Feb-2025/Length of Last Word 247965.py
@@ -0,0 +1,7 @@
+# Problem: Length of Last Word - https://leetcode.com/problems/length-of-last-word/description/
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        x=s.split()
+        length=len(x[-1])
+        return length
