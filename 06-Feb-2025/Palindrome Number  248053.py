884901cdbf24892fe459c0e2192b13e93b75c585
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-06 22:01:09 +0300

    Created 'Palindrome Number ' by A2SV Hub

---
+++ 06-Feb-2025/Palindrome Number  248053.py
@@ -0,0 +1,15 @@
+# Problem: Palindrome Number  - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x <0:
+            return False 
+        left=-0
+        right=len(str(x))-1
+        x=str(x)
+        while left < right:
+            if x[left]!=x[right]:
+                return False 
+            left+=1
+            right-=1
+        return True
