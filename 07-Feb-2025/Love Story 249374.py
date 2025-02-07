2fc1e2e078f6e338ee75b58045a8e524f5818d40
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-07 11:59:54 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 07-Feb-2025/Love Story 249374.py
@@ -0,0 +1,17 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+def love(s):
+    count = 0
+    for i in range(len(st)):
+        if s[i] != st[i]:
+            count += 1 
+    print(count)
+
+
+if __name__ == "__main__":
+    t = int(input())
+    arr = []
+    st = "codeforces"
+    for i in range(t):
+        s = input()
+        love(s)
