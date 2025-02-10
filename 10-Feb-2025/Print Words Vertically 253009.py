39870b972af9d582560ea60844bcfe9d1e5bbd8f
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-10 11:58:36 +0300

    Created 'Print Words Vertically' by A2SV Hub

---
+++ 10-Feb-2025/Print Words Vertically 253009.py
@@ -0,0 +1,24 @@
+# Problem: Print Words Vertically - https://leetcode.com/problems/print-words-vertically/description/
+
+class Solution:
+    def printVertically(self, s: str) -> List[str]:
+        list_t=[]
+        str_t=""
+        value=list(s.split())
+        max_value=0
+        for i in value:
+            max_value=max(max_value,len(i))
+
+        for index in range (max_value):
+            # use index as outer loop
+            str_t=""
+            for word in value:
+                if index>=len(word):
+                    str_t+=" "
+                else:
+                    str_t+=word[index]
+            str_t=str_t.rstrip()
+            list_t.append(str_t)
+        return list_t
+        
+
