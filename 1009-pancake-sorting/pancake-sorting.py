7b3b7d799bdb07c4689ca157b671d86b64daf034
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-11 23:32:08 +0300

    Time: 3 ms (69.82%) | Memory: 17.8 MB (61.52%) - LeetSync

---
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        list_t=[]
+        for i in range(len(arr),1,-1):
+            index=arr.index(i)
+
+            if index +1==i:
+                continue
+            if index!=0:
+                list_t.append(index+1)
+            list_t.append(i)
+            arr[:index+1]=reversed(arr[:index+1])
+            arr[:i]=reversed(arr[:i])
+        return list_t
+
