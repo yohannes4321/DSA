f66fbb65dc1cb199e8a918401691e66287bc2d32
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-11 23:52:12 +0300

    Created 'Selection Sort' by A2SV Hub

---
+++ 11-Feb-2025/Selection Sort 258354.py
@@ -0,0 +1,11 @@
+# Problem: Selection Sort - https://practice.geeksforgeeks.org/problems/selection-sort/1
+
+class Solution: 
+    def selectionSort(self, arr):
+        #code here
+        for i in range(len(arr)):
+            min_index=i
+            for j in range(i+1,len(arr)):
+                if arr[j]<arr[min_index]:
+                    min_index=j
+            arr[i],arr[min_index]=arr[min_index],arr[i]
