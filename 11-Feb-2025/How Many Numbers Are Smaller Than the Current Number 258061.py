4bac30a8292e72288001028424a4db4b05862a28
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-11 22:15:20 +0300

    Created 'How Many Numbers Are Smaller Than the Current Number' by A2SV Hub

---
+++ 11-Feb-2025/How Many Numbers Are Smaller Than the Current Number 258061.py
@@ -0,0 +1,17 @@
+# Problem: How Many Numbers Are Smaller Than the Current Number - https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        list_t=[]
+        
+        def search(val,array):
+            sum_value=0
+            for i in range(len(nums)):
+                if val > nums[i]:
+                    sum_value +=1
+            list_t.append(sum_value)
+            return list_t
+        for i in range(len(nums)):
+            s=search(nums[i],nums)
+            print(s)
+        return s
