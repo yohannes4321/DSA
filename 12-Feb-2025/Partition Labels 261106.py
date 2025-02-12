c1595e81a8e3a14e1e3df32bd821ad54806cd35c
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-12 19:43:23 +0300

    Created 'Partition Labels' by A2SV Hub

---
+++ 12-Feb-2025/Partition Labels 261106.py
@@ -0,0 +1,20 @@
+# Problem: Partition Labels - https://leetcode.com/problems/partition-labels/
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        occur = defaultdict(int)
+        for i in range(len(s) - 1 , -1 , -1):
+            if s[i] not in occur:
+                occur[s[i]] = i
+        ans = [0]
+        currMax = occur[s[0]]
+        for i in range(len(s)):
+            currMax = max(currMax , occur[s[i]])
+            if currMax == i:
+                ans.append(i + 1 - sum(ans))
+                print(s[:i+1] , i)
+
+           
+        return ans[1:]
+
+
