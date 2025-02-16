253ffd63fb01bf0de4f1f67da9f4588f196fef21
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-17 00:33:38 +0300

    Created 'Books' by A2SV Hub

---
+++ 16-Feb-2025/Books 268025.py
@@ -0,0 +1,18 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n,m=map(int,input().split())
+nums=list(map(int,input().split()))
+def Book(nums,n,m):
+    sum_value=0
+    left=0
+    max_length=0
+    for i in range(n):
+        sum_value+=nums[i]
+        while sum_value >m:
+            sum_value-=nums[left]
+            left+=1
+        max_length=max(max_length,i-left+1)
+            
+
+    return max_length
+print(Book(nums,n,m))
