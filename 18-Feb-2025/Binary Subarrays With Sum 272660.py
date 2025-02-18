bebe2ed6b0202522f216530e1a73b7c2d81fa29d
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-19 00:22:31 +0300

    Created 'Binary Subarrays With Sum' by A2SV Hub

---
+++ 18-Feb-2025/Binary Subarrays With Sum 272660.py
@@ -0,0 +1,16 @@
+# Problem: Binary Subarrays With Sum - https://leetcode.com/problems/binary-subarrays-with-sum/
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        count=0
+        prefix=0
+        freq={0:1}
+        for i in range(len(nums)):
+            prefix+=nums[i]
+            if prefix-goal in freq:
+                count+=freq[prefix-goal]
+            if prefix not  in freq:
+                freq[prefix]=1
+            else :
+                freq[prefix]+=1
+        return count 
