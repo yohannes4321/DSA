3472e29748375b93a3a9d489a98911494edf52b0
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 15:47:11 +0300

    Created 'Image Smoother' by A2SV Hub

---
+++ 21-Feb-2025/Image Smoother 278597.py
@@ -0,0 +1,16 @@
+# Problem: Image Smoother - https://leetcode.com/problems/image-smoother/description/
+
+class Solution(object):
+    def imageSmoother(self, img):
+        m, n = len(img), len(img[0])
+        result = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                neighbour = [img[x][y]
+                             for x in (i-1, i, i+1)
+                             for y in (j-1, j, j+1)
+                             if 0 <= x < m and 0 <= y < n]
+                result[i][j] = sum(neighbour) // len(neighbour)
+
+        return result
