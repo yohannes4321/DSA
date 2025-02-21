cf2ebdfaa2a79b14c872df499030bcb39ae1712d
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 15:55:39 +0300

    Created 'Sum of Even Numbers After Queries' by A2SV Hub

---
+++ 21-Feb-2025/Sum of Even Numbers After Queries 278617.py
@@ -0,0 +1,19 @@
+# Problem: Sum of Even Numbers After Queries - https://leetcode.com/problems/sum-of-even-numbers-after-queries/description/
+
+class Solution:
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        sum_even = sum(num for num in nums if num % 2 == 0)
+        result = []
+
+        for value, index in queries:
+            if nums[index] % 2 == 0:
+                sum_even -= nums[index]
+
+            nums[index] += value
+
+            if nums[index] % 2 == 0:
+                sum_even += nums[index]
+
+            result.append(sum_even)
+
+        return result
