0aef53ffc52f5bafeaf82b4530dcec4ad6cf13b7
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 19:36:47 +0300

    Created 'Majority Element II' by A2SV Hub

---
+++ 21-Feb-2025/Majority Element II 279042.py
@@ -0,0 +1,17 @@
+# Problem: Majority Element II - https://leetcode.com/problems/majority-element-ii/?envType=daily-question&envId=2023-10-05
+
+class Solution:
+    def majorityElement(self, nums: list[int]) -> list[int]:
+        # Create a Counter to store the count of each element
+        element_count = Counter(nums)
+        
+        majority_elements = []
+        threshold = len(nums) // 3
+        
+        # Iterate through the element count to identify majority elements
+        for element, count in element_count.items():
+            # Check if the element count is greater than the threshold
+            if count > threshold:
+                majority_elements.append(element)
+        
+        return majority_elements
