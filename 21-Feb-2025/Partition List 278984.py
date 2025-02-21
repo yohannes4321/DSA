510500d395ca429b2d43093d275eb5b1e25c6fca
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 19:21:53 +0300

    Created 'Partition List' by A2SV Hub

---
+++ 21-Feb-2025/Partition List 278984.py
@@ -0,0 +1,24 @@
+# Problem: Partition List - https://leetcode.com/problems/partition-list/
+
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        # Create dummy nodes for the left and right partitions
+        left_dummy = ListNode()
+        right_dummy = ListNode()
+        left_tail, right_tail = left_dummy, right_dummy
+
+        # Traverse the original list
+        while head:
+            if head.val < x:
+                left_tail.next = head
+                left_tail = left_tail.next
+            else:
+                right_tail.next = head
+                right_tail = right_tail.next
+            head = head.next
+
+        # Connect the left and right partitions
+        left_tail.next = right_dummy.next
+        right_tail.next = None
+
+        return left_dummy.next
