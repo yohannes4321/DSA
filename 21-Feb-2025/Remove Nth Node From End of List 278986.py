06acac11a484837f6531fa39b2c1158e8f3f2449
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 19:22:06 +0300

    Created 'Remove Nth Node From End of List' by A2SV Hub

---
+++ 21-Feb-2025/Remove Nth Node From End of List 278986.py
@@ -0,0 +1,22 @@
+# Problem: Remove Nth Node From End of List - https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    #this idea is mind blowing i canot think but 
+    # the secret is we gona have two pointers slow and fast 
+    # 2 steps from  the last element means we will make right pointer 2 + fast and slow start from the beggineing
+    #when the finsh the fast go to last element the slow come in the one node before the deleting element
+        dumy_node=ListNode(None,head)
+        fast=slow=dumy_node
+        for i in range(n):
+            fast=fast.next
+        while fast.next:
+            fast=fast.next
+            slow=slow.next
+        slow.next=slow.next.next
+        return dumy_node.next
