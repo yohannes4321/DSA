cb41c8d28299c53d903509ea24f6d55611b8df61
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 19:34:11 +0300

    Time: 96 ms (30.47%) | Memory: 18.5 MB (7.10%) - LeetSync

---
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def findRestaurant(self, list1, list2):
+        """
+        :type list1: List[str]
+        :type list2: List[str]
+        :rtype: List[str]
+        """
+         
+        mydict={}
+        for i in range(len(list1)):
+            if list1[i] in list2:
+                mydict[list1[i]]=i+list2.index(list1[i])
+        mydict=sorted(mydict.items(),key=lambda x:x[1])
+        print(mydict)
+        l=[]
+        z=mydict[0][1]
+        for i in range(len(mydict)):
+            if mydict[i][1]==z:
+                l.append(mydict[i][0])
+            else:
+                break
+        return l
+        
+        
+        
+            
+
+
+
+ 
+        
