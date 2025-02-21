daa6215867b2c2e09b2ff53d90597e50eeb80995
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 19:24:21 +0300

    Created 'Reverse Linked List' by A2SV Hub

---
+++ 21-Feb-2025/Reverse Linked List 278993.py
@@ -0,0 +1,19 @@
+# Problem: Reverse Linked List - https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current=head
+        previous=None
+        while current :
+            
+            front=current.next
+            current.next=previous
+            previous=current
+            current=front
+            
+        return previous
