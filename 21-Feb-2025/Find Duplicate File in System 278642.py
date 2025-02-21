c31b34ecdabf5c1a9dd8f49bbb53bafb6fdb3143
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 16:09:00 +0300

    Created 'Find Duplicate File in System' by A2SV Hub

---
+++ 21-Feb-2025/Find Duplicate File in System 278642.py
@@ -0,0 +1,32 @@
+# Problem: Find Duplicate File in System - https://leetcode.com/problems/find-duplicate-file-in-system/
+
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        map={}
+        for path in paths:
+            path = path.split()
+ 
+            for values in path[1:]:
+                string = str(path[0])
+                print(values)                
+                key  = ""
+                i = 0
+                while i < len(values):
+                    if values[i] == "(":
+                        string+= "/" + values[:i]
+                        i+=1
+                        while i < len(values) and i != ")":
+                            key+=values[i]
+                            i+=1
+                    i+=1
+                if key not in map:
+                    map[key] = [string]
+                else:
+                    map[key].append(string)
+        liste = []
+        for key,values in map.items():
+            if len(values) > 1:
+                liste.append(values)
+        return liste
+
+        
