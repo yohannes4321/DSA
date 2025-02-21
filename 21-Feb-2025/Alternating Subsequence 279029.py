1fc1f5d269d22e706fbfd2364b482bf1f47a59d8
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 19:31:31 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 21-Feb-2025/Alternating Subsequence 279029.py
@@ -0,0 +1,18 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+t=int(input())
+while t>0:
+    sum=0
+    t-=1
+    n=int(input())
+    l=list(map(int,input().split()))
+    temp=[0]*n
+    for i in range(n):
+        if i==0 or l[i]*l[i-1]<0:             
+            temp[i]=l[i]
+            if i!=0 and l[i]*l[i-1]:
+                sum+=temp[i-1]
+        else:
+            temp[i]=l[i]=max(l[i],l[i-1])
+    sum+=temp[-1]
+    print(sum)
