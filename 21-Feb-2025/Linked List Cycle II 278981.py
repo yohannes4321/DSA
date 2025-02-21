af2a03972d7e0e6420582d032aa58a3e3aa443ca
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-02-21 19:21:24 +0300

    Created 'Linked List Cycle II' by A2SV Hub

---
+++ 21-Feb-2025/Linked List Cycle II 278981.py
@@ -0,0 +1,18 @@
+# Problem: Linked List Cycle II - https://leetcode.com/problems/linked-list-cycle-ii/
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+
+        # Check if there's a cycle
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow=head
+                while slow!=fast:
+                    slow=slow.next
+                    fast=fast.next
+                return slow
+        return None
+    
