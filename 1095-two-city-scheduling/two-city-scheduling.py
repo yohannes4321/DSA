00cfb590bfb5b3530bc98b678cafd35c1b01e1c5
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-03-03 20:04:00 +0300

    Time: 4 ms (12.60%) | Memory: 18.1 MB (12.19%) - LeetSync

---
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        
+        nums2 = sorted(costs,key=lambda x:x[0]-x[1])
+        cur=0
+        print(nums2)
+        for i in range(len(costs)//2):
+            cur+=nums2[i][0]
+        for i in range(len(costs)//2,len(costs)):
+            cur+=nums2[i][1]
+        return cur
