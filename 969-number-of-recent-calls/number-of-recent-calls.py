c20611431712deeb9a81b84fa2cee5ac7a20c818
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-03-11 04:44:37 +1200

    Time: 31 ms (82.74%) | Memory: 22.9 MB (98.81%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -0,0 +1,16 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.queue=[]
+
+    def ping(self, t: int) -> int:
+
+        while self.queue and  t-self.queue[0] >3000:
+            self.queue.pop(0)
+        self.queue.append(t)
+        return len(self.queue)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
