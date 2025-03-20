65377b77a1b58c5d59c4358e34c86eb849da47a1
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-03-21 00:47:29 +1200

    Time: 60 ms (5.24%) | Memory: 18 MB (92.77%) - LeetSync

---
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfSubtree(self, root: TreeNode) -> int:
+        count=0
+        def helper(root):
+            nonlocal count
+            if root is None:
+                return 0,0
+            left_value=helper(root.left )
+            right_value=helper(root.right )
+            print("left_value",left_value)
+            print("right_value",right_value)
+            sum_value=left_value[0]+right_value[0]+root.val
+            number_of_node=left_value[1]+right_value[1]+1
+            if root.val==sum_value//number_of_node:
+                count+=1
+            return sum_value,number_of_node
+        helper(root)
+        
+        return count 
+
+
+
