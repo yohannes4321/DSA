ce0598c1bf05a8e4afd08deb4d82723909ea8d8b
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-03-21 01:01:50 +1200

    Time: 0 ms (100.00%) | Memory: 18 MB (40.47%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack=[]
+        for i in range(len(logs)):
+            if logs[i]=="../":
+                if stack:
+
+                    stack.pop()
+            elif logs[i]=="./":
+                pass
+            else:
+                stack.append(logs[i])
+        print("stack",stack)
+        return len(stack)
