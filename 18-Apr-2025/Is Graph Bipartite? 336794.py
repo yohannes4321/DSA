b8b37bad4a4d877fd96ead36f814f43a7691cdc9
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-04-18 00:16:01 -0800

    Created 'Is Graph Bipartite?' by A2SV Hub

---
+++ 18-Apr-2025/Is Graph Bipartite? 336794.py
@@ -0,0 +1,28 @@
+# Problem: Is Graph Bipartite? - https://leetcode.com/problems/is-graph-bipartite/
+
+from collections import deque
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        color=[0] *(len(graph))
+        queue=deque()
+        def bfs(i):
+            queue.append(i)
+            color[i]=1
+            while queue:
+                node=queue.popleft()
+                for neigh in graph[node]:
+                    print(neigh)
+                    if color[neigh]==0:
+                        color[neigh]=- color[node]
+                        queue.append(neigh)
+                    elif color[neigh]== color[node]:
+                        print("gorbert",color[neigh])
+                        print("real node",color[node])
+                        return False
+            return True
+                
+        for i in range(len(graph)):
+            if color[i] ==0:
+                if not bfs(i):
+                    return False
+        return True
