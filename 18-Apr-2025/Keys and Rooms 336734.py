6a073162a1dba1cf8937aef51420983de82827fe
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-04-17 21:09:16 -0800

    Created 'Keys and Rooms' by A2SV Hub

---
+++ 18-Apr-2025/Keys and Rooms 336734.py
@@ -0,0 +1,26 @@
+# Problem: Keys and Rooms - https://leetcode.com/problems/keys-and-rooms/
+
+from collections import deque
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        queue=deque() 
+        visted=set()
+        adj_list=[]
+        for i in range(len(rooms)):
+            # print(i)
+            # src,des=i
+            adj_list.append(rooms[i])
+        # print(adj_list)
+        queue.append(0)
+        visted.add(0)
+        while queue:
+            room=queue.popleft()
+            for niegh in adj_list[room]:
+                # print(niegh)
+                if niegh not in visted:
+                    queue.append(niegh)
+                    visted.add(niegh)
+        if (len(adj_list))==len(visted):
+            return True
+        else:
+            return False
