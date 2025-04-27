481dc660def2990b2a74428740dce888afc3419d
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2025-04-26 16:51:10 -0800

    Created 'Couse Schedule' by A2SV Hub

---
+++ 27-Apr-2025/Couse Schedule 343247.py
@@ -0,0 +1,35 @@
+# Problem: Couse Schedule - https://leetcode.com/problems/course-schedule/
+
+from collections import deque
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        queue=deque()
+        adj_list=[[] for _ in range(numCourses)]
+        indegree=[0]*numCourses
+        for des,src in prerequisites:
+            adj_list[src].append(des)
+            # adj_list[des].append(src)
+            indegree[des]+=1
+            # indegree[src]+=1
+        # print(indegree)
+        for i in range(len(indegree)):
+            if indegree[i]==0:
+                queue.append(i)
+
+        res=[]
+        while queue:
+            node=queue.popleft()
+            res.append(node)
+            # print(res)
+
+            for neigh in adj_list[node]:
+                indegree[neigh]-=1
+                # indegree[node]-=1
+                # print("in",indegree[neigh])
+                if indegree[neigh]==0:
+                    queue.append(neigh)
+        # print(res)
+        if len(res)!=numCourses:
+            return False
+        else:
+            return True
